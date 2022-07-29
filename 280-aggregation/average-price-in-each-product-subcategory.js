d43963e61a5d89a7a869ee6de03e3db03ac54048
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-07-29 16:01:02 -0700

    Diberry/agg2 (#11)
    * Updates to aggregation
    
    * edits

--- 280-aggregation/average-price-in-each-product-subcategory.js
+++ 280-aggregation/average-price-in-each-product-subcategory.js
@@ -6,107 +6,79 @@
 // the number of products in that subcategory.
 // Sort by average price descending.
 
-/*
-// <console_result>
-{"_id":"Bikes, Touring Bikes","averagePrice":1425.25,"nProducts":22}
-{"_id":"Components, Saddles","averagePrice":39.63,"nProducts":9}
-{"_id":"Accessories, Bottles and Cages","averagePrice":7.99,"nProducts":3}
-{"_id":"Accessories, Tires and Tubes","averagePrice":19.48,"nProducts":11}
-{"_id":"Components, Pedals","averagePrice":64.02,"nProducts":7}
-{"_id":"Clothing, Tights","averagePrice":74.99,"nProducts":3}
-{"_id":"Components, Bottom Brackets","averagePrice":92.24,"nProducts":3}
-{"_id":"Clothing, Gloves","averagePrice":31.24,"nProducts":6}
-{"_id":"Clothing, Vests","averagePrice":63.5,"nProducts":3}
-{"_id":"Components, Headsets","averagePrice":87.07,"nProducts":3}
-{"_id":"Components, Forks","averagePrice":184.4,"nProducts":3}
-{"_id":"Components, Road Frames","averagePrice":780.04,"nProducts":33}
-{"_id":"Components, Wheels","averagePrice":220.93,"nProducts":14}
-{"_id":"Components, Mountain Frames","averagePrice":678.25,"nProducts":28}
-{"_id":"Accessories, Lights","averagePrice":31.32,"nProducts":3}
-{"_id":"Clothing, Jerseys","averagePrice":51.99,"nProducts":8}
-{"_id":"Bikes, Mountain Bikes","averagePrice":1683.36,"nProducts":32}
-{"_id":"Bikes, Road Bikes","averagePrice":1597.45,"nProducts":43}
-{"_id":"Components, Chains","averagePrice":20.24,"nProducts":1}
-{"_id":"Accessories, Locks","averagePrice":25,"nProducts":1}
-{"_id":"Components, Touring Frames","averagePrice":631.42,"nProducts":18}
-{"_id":"Clothing, Socks","averagePrice":9.25,"nProducts":4}
-{"_id":"Accessories, Panniers","averagePrice":125,"nProducts":1}
-{"_id":"Components, Brakes","averagePrice":106.5,"nProducts":2}
-{"_id":"Components, Handlebars","averagePrice":73.89,"nProducts":8}
-{"_id":"Components, Cranksets","averagePrice":278.99,"nProducts":3}
-{"_id":"Accessories, Bike Racks","averagePrice":120,"nProducts":1}
-{"_id":"Components, Derailleurs","averagePrice":106.48,"nProducts":2}
-{"_id":"Clothing, Shorts","averagePrice":64.28,"nProducts":7}
-{"_id":"Accessories, Bike Stands","averagePrice":159,"nProducts":1}
-{"_id":"Accessories, Pumps","averagePrice":22.49,"nProducts":2}
-{"_id":"Accessories, Hydration Packs","averagePrice":54.99,"nProducts":1}
-{"_id":"Clothing, Bib-Shorts","averagePrice":89.99,"nProducts":3}
-{"_id":"Accessories, Helmets","averagePrice":34.99,"nProducts":3}
-{"_id":"Accessories, Cleaners","averagePrice":7.95,"nProducts":1}
-{"_id":"Accessories, Fenders","averagePrice":21.98,"nProducts":1}
-{"_id":"Clothing, Caps","averagePrice":8.99,"nProducts":1}
-// </console_result>
-*/
-
 // <package_dependencies> 
 // Read .env file and set environment variables
 require('dotenv').config();
 
 // Use official mongodb driver to connect to the server
 const { MongoClient } = require('mongodb');
-// </package_dependencies>
 
-// <client_credentials> 
 // New instance of MongoClient with connection string
 // for Cosmos DB
 const url = process.env.COSMOS_CONNECTION_STRING;
 const client = new MongoClient(url);
-// </client_credentials>
 
 async function main() {
 
   try {
-    // <connect_client>
+
     // Use connect method to connect to the server
     await client.connect();
-    // </connect_client>
 
-    // <execute_aggregation_pipeline> 
-    const pipeline = [
-      {
-        '$group': {
-          '_id': '$categoryName',
-          'averagePrice': {
-            '$avg': '$price'
-          },
-          'countOfProducts': {
-            '$sum': 1
-          }
+    // Group all products by category
+    // Find average price of each category
+    // Count # of products in each category
+    const groupByCategory = {
+      '$group': {
+        '_id': '$categoryName',
+        'averagePrice': {
+          '$avg': '$price'
         },
-      },
-      {
-        '$project': {
-          "averagePrice": { '$round': ["$averagePrice", 2] },
-          'nProducts':'$countOfProducts'
+        'countOfProducts': {
+          '$sum': 1
         }
       },
-      { '$sort': { "$averagePrice": -1 } },
+    };
 
+    // Round price to 2 decimal places
+    // Don't return _id
+    // Rename category name help in `_id` to `categoryName`
+    // Round prices to 2 decimal places
+    // Rename property for countOfProducts to nProducts
+    const additionalTransformations = {
+      '$project': {
+        '_id': 0,
+        'category': '$_id',
+        'nProducts':'$countOfProducts',
+        'averagePrice': { '$round': ['$averagePrice', 2] }
+      }
+    };
+
+    // Sort by average price descending
+    const sort = { '$sort': { '$averagePrice': -1 } };
+
+    // stages execute in order from top to bottom
+    const pipeline = [
+      groupByCategory,
+      additionalTransformations,
+      sort
     ];
 
-    const aggCursor = client.db("adventureworks").collection('products').aggregate(pipeline);
+    const db = 'adventureworks';
+    const collection = 'products';
 
+    // Get iterable cursor
+    const aggCursor = client.db(db).collection(collection).aggregate(pipeline);
+
+    // Display each item in cursor
     await aggCursor.forEach(product => {
       console.log(JSON.stringify(product));
     });
-    // </execute_aggregation_pipeline> 
 
-    return "done";
+    return 'done';
   } catch (err) {
     console.log(JSON.stringify(err));
   }
-
-
 }
 
 main()
@@ -117,3 +89,15 @@ main()
     client.close()
   });
 
+// Results:
+// {"averagePrice":51.99,"category":"Clothing, Jerseys","nProducts":8}
+// {"averagePrice":1683.36,"category":"Bikes, Mountain Bikes","nProducts":32}
+// {"averagePrice":1597.45,"category":"Bikes, Road Bikes","nProducts":43}
+// {"averagePrice":20.24,"category":"Components, Chains","nProducts":1}
+// {"averagePrice":25,"category":"Accessories, Locks","nProducts":1}
+// {"averagePrice":631.42,"category":"Components, Touring Frames","nProducts":18}
+// {"averagePrice":9.25,"category":"Clothing, Socks","nProducts":4}
+// {"averagePrice":125,"category":"Accessories, Panniers","nProducts":1}
+// ... remaining fields ...
+
+// </aggregation_1>
--- 280-aggregation/bike-types-and-price-ranges.js
+++ 280-aggregation/bike-types-and-price-ranges.js
@@ -2,99 +2,115 @@
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 // ------------------------------------------------------------
 
+// < aggregation_1>
 // Goal: Find the price range for the different bike subcategories. 
 
-/*
-// <console_result>
-{"_id":"Mountain Bikes","nProducts":32,"min":539.99,"avg":1683.37,"max":3399.99}
-{"_id":"Road Bikes","nProducts":43,"min":539.99,"avg":1597.45,"max":3578.27}
-{"_id":"Touring Bikes","nProducts":22,"min":742.35,"avg":1425.25,"max":2384.07}
-// </console_result>
-*/
-
-// <package_dependencies> 
 // Read .env file and set environment variables
 require('dotenv').config();
 
 // Use official mongodb driver to connect to the server
 const { MongoClient } = require('mongodb');
-// </package_dependencies>
 
-// <client_credentials> 
 // New instance of MongoClient with connection string
 // for Cosmos DB
 const url = process.env.COSMOS_CONNECTION_STRING;
 const client = new MongoClient(url);
-// </client_credentials>
 
 async function main() {
 
   try {
-    // <connect_client>
+
     // Use connect method to connect to the server
     await client.connect();
-    // </connect_client>
 
-    // <execute_aggregation_pipeline> 
-    const pipeline = [
-      {
-        '$match': {
-          'categoryName': { $regex: 'Bikes' },
-        }
-      },
-      {
-        $addFields: {
-          'categories': { '$split': ['$categoryName', ', '] }
-        }
-      },
+    const categoryName = 'Bikes';
 
-      {
-        $addFields: {
-          'subcategory': { '$slice': ["$categories", 1, { $subtract: [{ $size: "$categories" }, 1] }] }
-        }
-      },
-      {
-        '$group': {
-          '_id': '$subcategory',
-          'maxPrice': {
-            '$max': '$price'
-          },
-          'averagePrice': {
-            '$avg': '$price'
-          },
-          'minPrice': {
-            '$min': '$price'
-          },
-          'countOfProducts': {
-            '$sum': 1
-          }
+    const findAllBikes = {
+      '$match': {
+        'categoryName': { $regex:  categoryName},
+      }
+    };
+
+    // Convert 'Bikes, Touring Bikes' to ['Bikes', 'Touring Bikes']
+    const splitStringIntoCsvArray = {
+      $addFields: {
+        'categories': { '$split': ['$categoryName', ', '] }
+      }
+    };
+
+    // Remove first element from array
+    // Converts ['Bikes', 'Touring Bikes'] to ['Touring Bikes']
+    const removeFirstElement = {
+      $addFields: {
+        'subcategory': { '$slice': ['$categories', 1, { $subtract: [{ $size: '$categories' }, 1] }] }
+      }
+    }
+
+    // Group items by book subcategory, and find min, avg, and max price
+    const groupBySubcategory = {
+      '$group': {
+        '_id': '$subcategory',
+        'maxPrice': {
+          '$max': '$price'
         },
-      },
-      {
-        '$project': {
-          'nProducts': '$countOfProducts',
-          '_id': { '$arrayElemAt': ['$_id', 0]},
-          'min': { '$round': ["$minPrice", 2] },
-          'avg': { '$round': ["$averagePrice", 2] },
-          'max': { '$round': ["$maxPrice", 2] }
+        'averagePrice': {
+          '$avg': '$price'
+        },
+        'minPrice': {
+          '$min': '$price'
+        },
+        'countOfProducts': {
+          '$sum': 1
         }
       },
-      { '$sort': { "_id": 1 } },
+    };
+
+    // Miscellaneous transformations
+    // Don't return _id
+    // Convert subcategory from array of 1 item to string in `name`
+    // Round prices to 2 decimal places
+    // Rename property for countOfProducts to nProducts
+    const additionalTransformations = {
+      '$project': {
+        '_id': 0,
+        'name': { '$arrayElemAt': ['$_id', 0]},
+        'nProducts': '$countOfProducts',
+        'min': { '$round': ['$minPrice', 2] },
+        'avg': { '$round': ['$averagePrice', 2] },
+        'max': { '$round': ['$maxPrice', 2] }
+      }
+    };
+
+    // Sort by subcategory
+    const sortBySubcategory = { '$sort': 
+        { 'name': 1 } 
+    };
+
+    // stages execute in order from top to bottom
+    const pipeline = [
+      findAllBikes,
+      splitStringIntoCsvArray,
+      removeFirstElement,
+      groupBySubcategory,
+      additionalTransformations,
+      sortBySubcategory
     ];
 
-    const aggCursor = client.db("adventureworks").collection('products').aggregate(pipeline);
+    const db = 'adventureworks';
+    const collection = 'products';
 
+    // Get iterable cursor
+    const aggCursor = client.db(db).collection(collection).aggregate(pipeline);
+
+    // Display each item in cursor
     await aggCursor.forEach(product => {
       console.log(JSON.stringify(product));
     });
-    // </execute_aggregation_pipeline> 
 
-    return "done";
+    return 'done';
   } catch (err) {
     console.log(JSON.stringify(err));
   }
-
-
 }
 
 main()
@@ -102,5 +118,12 @@ main()
   .catch(console.error)
   .finally(() => {
     // Close the db and its underlying connections
-    client.close()
+    client.close();
   });
+
+// Results: 
+// {'name':'Mountain Bikes','nProducts':32,'min':539.99,'avg':1683.37,'max':3399.99}
+// {'name':'Road Bikes','nProducts':43,'min':539.99,'avg':1597.45,'max':3578.27}
+// {'name':'Touring Bikes','nProducts':22,'min':742.35,'avg':1425.25,'max':2384.07}
+
+// </aggregation_1>
